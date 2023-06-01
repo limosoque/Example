d5c95a4fc25e235d35c6c9d59cf7f53ed8acb0f2
limosoque <106828150+limosoque@users.noreply.github.com>
2023-06-01 19:57:43 +0300

    Update App.py

--- App.py
+++ App.py
@@ -1,9 +1,19 @@
 from binary_search import binary_search
 
-print("Введите через пробел числа, среди которых будет производиться поиск")
-arr = [int(x) for x in input().split()]
-print("Введите через пробел числа, которые хотите найти")
-find = [int(x) for x in input().split()]
+help = "Запустить программу: 1", "\n", "Завершить программу: 2", "\n", "Справка: 3"
+command = 1
+print(help)
+while True:
+    command = int(input())
+    if command == 2:
+        break
+    if command == 3:
+        print(help)
+        continue
+    print("Введите через пробел числа, среди которых будет производиться поиск")
+    arr = [int(x) for x in input().split()]
+    print("Введите через пробел числа, которые хотите найти")
+    find = [int(x) for x in input().split()]
 
-for to_find in find:
-    print(binary_search(arr, to_find))
+    for to_find in find:
+        print(binary_search(arr, to_find))
