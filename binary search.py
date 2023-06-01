e7aa72921e6c7a7a3e00c6a7e91a8b04bd13825f
limosoque <106828150+limosoque@users.noreply.github.com>
2023-06-01 16:18:55 +0300

    Update binary search.py

--- binary search.py
+++ binary search.py
@@ -1,19 +1,19 @@
-def binary_search(arr: list, find:int) -> str:
+def binary_search(arr: list, find:int) -> bool:
     left, right = 0, len(arr) - 1
     while left <= right:
         middle = (left + right) // 2
         if arr[middle] == find:
-            return "YES"
+            return True
         if arr[middle] > find:
             right = middle - 1
         else:
             left = middle + 1
-    return "NO"
+    return False
 
 
-arr_size, find_count = [x for x in input().split()]
-arr = [int(x) for x in input().split()]
-find = [int(x) for x in input().split()]
-
-for to_find in find:
-    print(binary_search(arr, to_find))
+# arr_size, find_count = [x for x in input().split()]
+# arr = [int(x) for x in input().split()]
+# find = [int(x) for x in input().split()]
+#
+# for to_find in find:
+#     print(binary_search(arr, to_find))
